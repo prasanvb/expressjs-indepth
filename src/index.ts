1aeca2a958d29b9637439a9d27b2cb7f760c0026
prasanv <prasan.venkatb@gmail.com>
2024-09-29 10:52:43 -0700

    feat: added user api and updated eslint

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,22 @@
 import express, { Request, Response } from 'express';
-import 'dotenv/config'
+import 'dotenv/config';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello, TypeScript Express!');
+  res.send('Express-TypeScript-Indepth!!');
+});
+
+app.get('/api/users', (req: Request, res: Response) => {
+  console.log({ req });
+  res.send([
+    { id: 1, firstname: 'prasan', lastname: 'bala' },
+    { id: 2, firstname: 'ganesh', lastname: 'siva' },
+    { id: 3, firstname: 'karthikeya', lastname: 'siva' },
+  ]);
 });
 
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
