07f4dd56faec72990ad8e7fd862d195b406eb8b2
prasanv <prasan.venkatb@gmail.com>
2024-09-29 11:16:23 -0700

    feat: add route params

--- src/index.ts
+++ src/index.ts
@@ -4,17 +4,36 @@ import 'dotenv/config';
 const app = express();
 const port = process.env.PORT || 3000;
 
+const userList = [
+  { id: 1, firstname: 'prasan', lastname: 'bala' },
+  { id: 2, firstname: 'ganesh', lastname: 'siva' },
+  { id: 3, firstname: 'karthikeya', lastname: 'siva' },
+];
 app.get('/', (req: Request, res: Response) => {
   res.send('Express-TypeScript-Indepth!!');
 });
 
 app.get('/api/users', (req: Request, res: Response) => {
-  console.log({ req });
-  res.send([
-    { id: 1, firstname: 'prasan', lastname: 'bala' },
-    { id: 2, firstname: 'ganesh', lastname: 'siva' },
-    { id: 3, firstname: 'karthikeya', lastname: 'siva' },
-  ]);
+  res.send(userList);
+});
+
+// api with route paramters
+// NOTE: route paramters returned by express is always a string
+app.get('/api/users/:id', (req: Request, res: Response) => {
+  // TODO: parseInt has a issues with route params like 2f, need to use regex
+  const parsedRouteParamId = parseInt(req.params.id);
+
+  if (isNaN(parsedRouteParamId)) {
+    res.status(400).send({ message: 'Bad Request. Invalid Id.' });
+  }
+
+  const user = userList.find((user) => user.id === parsedRouteParamId);
+
+  if (user === undefined) {
+    res.status(400).send({ message: 'Id not found.' });
+  }
+
+  res.send(user);
 });
 
 app.listen(port, () => {
